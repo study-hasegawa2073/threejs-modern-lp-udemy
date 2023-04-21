507ba2ce80f428c1b22a71d6198b838552441298
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-21 21:42:42 +0900

    ジオメトリを回転

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ mesh3.position.set(2, 0, -6);
 mesh4.position.set(5, 0, 3);
 
 scene.add(mesh1, mesh2, mesh3, mesh4);
+const meshes = [mesh1, mesh2, mesh3, mesh4];
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 4);
 directionalLight.position.set(0.5, 1, 0);
@@ -65,8 +66,18 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(window.devicePixelRatio);
 });
 
+const clock = new THREE.Clock();
+
 const animate = () => {
   renderer.render(scene, camera);
+
+  let getDeltaTime = clock.getDelta();
+
+  for (const mesh of meshes) {
+    mesh.rotation.x += 0.1 * getDeltaTime;
+    mesh.rotation.y += 0.12 * getDeltaTime;
+  }
+
   window.requestAnimationFrame(animate);
 };
 animate();
